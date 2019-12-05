af1aa0cbdbf8f2469ac4f5348c93b1877a543dc9
Shani Ginat <sharonlapidot1@gmail.com>
2019-12-05 14:12:30 +0200

    make it better

--- src/containers/Series/Series.js
+++ src/containers/Series/Series.js
@@ -17,7 +17,7 @@ export default class Series extends React.Component {
     handleChange = (e) =>{
         this.setState({seriesName: e.target.value , isFetching: true})
 
-        fetch(`http://api.tvmaze.com/search/shows?q=${ e.target.value}`)
+        fetch(`https://api.tvmaze.com/search/shows?q=${ e.target.value}`)
             .then((response)=> response.json())
             .then((json)=> this.setState({series: json , isFetching: false}));
     };
--- src/containers/SingleSeries/SingleSeries.js
+++ src/containers/SingleSeries/SingleSeries.js
@@ -10,7 +10,7 @@ export default class SingleSeries extends React.Component{
     componentDidMount() {
         const  id = this.props.match.params.id;
 
-        fetch(`http://api.tvmaze.com/shows/${id}?embed=episodes`)
+        fetch(`https://api.tvmaze.com/shows/${id}?embed=episodes`)
             .then((response)=> response.json())
             .then((json)=> this.setState({show: json}));
     };
